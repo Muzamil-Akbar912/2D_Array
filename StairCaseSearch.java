a02d1a3fb197db43d06bf39d79636f9d76cada32
Muzamil-Akbar912 <darmuzamil006@gmail.com>
2026-09-21 23:27:30 +0530

    Add new question

---
+++ StairCaseSearch.java
@@ -0,0 +1,49 @@
+public class StairCaseSearch {
+    // Search in sorted matrix case 01.....
+    public static boolean stairCaseSearchCase01(int matrix[][], int key){
+        int row = 0, col = matrix[0].length-1;
+        while (row < matrix.length && col >= 0) {
+            if (matrix[row][col] == key) {
+                System.out.println("Key found at ( "+ row +", "+ col +")");
+                return true;
+            }
+            else if (matrix[row][col] > key) {
+                col--;
+            } else {
+                row++;
+            }
+        }
+        System.out.println("Key not found");
+        return false;
+    }
+
+    // Search in sorted array case 02.....
+    public static boolean stairCaseSearchCase2(int matrix[][], int key){
+        int row = matrix.length-1;
+        int col = 0;
+        while (row >= 0 && col < matrix[0].length) {
+            if (matrix[row][col] == key) {
+                System.out.println("Key found at ( "+ row +", "+ col +")");
+                return true;
+            }
+            else if (matrix[row][col] > key) {
+                row--;
+            } else {
+                col++;
+            }
+        }
+        System.out.println("Key not found");
+        return false;
+    }
+    public static void main(String[] args) {
+        int matrix[][] = {
+            {10, 20, 30, 40},
+            {15, 25, 35, 45},
+            {27, 29, 37, 48},
+            {32, 33, 39, 50},
+        };
+        int key = 29;
+        stairCaseSearchCase01(matrix, key);
+        stairCaseSearchCase2(matrix, key);
+    }
+}
